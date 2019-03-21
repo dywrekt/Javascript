21592bef24e1fa892ca6ad32aa7b08b9d4c6de00
hicham <hicham.harmaz1@student.rocvf.nl>
2019-03-21 11:26:24 +0100

    javascript helemaal af

---
+++ src/h02/JS/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("titel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "IMG/aap1.jpg";
+document.getElementById("aap2").src = "IMG/aap2.jpg";
